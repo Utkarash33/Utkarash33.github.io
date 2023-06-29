bdb953dd2974a6665a198cb7a3ec5b48749fce89
atulthakur3303@gmail.com <atulthakur3303@gmail.com>
2023-06-29 22:16:01 +0530

    Add id's and classes

--- main.js
+++ main.js
@@ -10,3 +10,63 @@ const showMenu = (toggleId, navId) => {
     }
 }
 showMenu('nav-toggle', 'nav-menu')
+
+//===================Resume=================
+
+document.getElementById("resume2").onclick = function(){
+    let file = "assets/nikhilchauhan@resume.pdf";
+    let down  = document.createElement("a");
+    down.href = file;
+    down.download = file.substring(file.lastIndexOf("/")+1);
+    down.click();
+    let url = "https://drive.google.com/file/d/1E1GWpPqwZRcPCeI4pBCCyfnzDbfIdchn/view?usp=sharing";
+    window.open(url);
+
+}
+
+
+
+// 
+/*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction() {
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive() {
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active')
+        } else {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+/*===== SCROLL REVEAL ANIMATION =====*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 1500,
+    delay: 200,
+    //     reset: true
+});
+
+sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text', {});
+sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img, .about__details', { delay: 300 });
+sr.reveal('.home__social-icon', { interval: 200 });
+sr.reveal('.skills__data, .work__img, .contact__input', { interval: 200 }); 
