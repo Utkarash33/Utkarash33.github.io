f268b51a2151c0ad783e38b8b5a444afdaa89caa
atulthakur3303@gmail.com <atulthakur3303@gmail.com>
2023-07-10 23:01:45 +0530

    test'

--- main.js
+++ main.js
@@ -1,74 +1,84 @@
-
-
-// /*===== MENU SHOW =====*/
+// Menu show function
 const showMenu = (toggleId, navId) => {
-    const toggle = document.getElementById(toggleId),
-        nav = document.getElementById(navId)
-
+    const toggle = document.getElementById(toggleId);
+    const nav = document.getElementById(navId);
+  
     if (toggle && nav) {
-        toggle.addEventListener('click', () => {
-            nav.classList.toggle('show')
-        })
+      toggle.addEventListener('click', () => {
+        nav.classList.toggle('show');
+      });
     }
-}
-showMenu('nav-toggle', 'nav-menu')
-
-// //===================Resume=================
-
-
-function openResume() {
+  };
+  showMenu('nav-toggle', 'nav-menu');
+  
+  // Resume button click event listeners
+  const resumeButton1 = document.getElementById('resume-button-1');
+  const resumeButton2 = document.getElementById('resume-button-2');
+  const resumeLink1 = document.getElementById('resume-link-1');
+  const resumeLink2 = document.getElementById('resume-link-2');
+  
+  resumeButton1.addEventListener('click', openResume);
+  resumeButton2.addEventListener('click', openResume);
+  resumeLink1.addEventListener('click', (e) => {
+    e.preventDefault();
+    openResume();
+  });
+  resumeLink2.addEventListener('click', (e) => {
+    e.preventDefault();
+    openResume();
+  });
+  
+  function openResume() {
     // Open the resume in a new tab
-    window.open("assets/Utkarash-Thakur-Resume.pdf", "_blank");
+    window.open('assets/Utkarash-Thakur-Resume.pdf', '_blank');
   
     // Download the resume as a PDF
-    let downloadLink = document.createElement('a');
-    downloadLink.href = "assets/Utkarash-Thakur-Resume.pdf";
-    downloadLink.download = "Utkarash-Thakur-Resume.pdf";
+    const downloadLink = document.createElement('a');
+    downloadLink.href = 'assets/Utkarash-Thakur-Resume.pdf';
+    downloadLink.download = 'Utkarash-Thakur-Resume.pdf';
     downloadLink.click();
   }
   
-
-// // 
-// /*==================== REMOVE MENU MOBILE ====================*/
-const navLink = document.querySelectorAll('.nav__link')
-
-function linkAction() {
-    const navMenu = document.getElementById('nav-menu')
-    // When we click on each nav__link, we remove the show-menu class
-    navMenu.classList.remove('show')
-}
-navLink.forEach(n => n.addEventListener('click', linkAction))
-
-/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
-const sections = document.querySelectorAll('section[id]')
-
-function scrollActive() {
-    const scrollY = window.pageYOffset
-
-    sections.forEach(current => {
-        const sectionHeight = current.offsetHeight
-        const sectionTop = current.offsetTop - 50;
-        sectionId = current.getAttribute('id')
-
-        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active')
-        } else {
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active')
-        }
-    })
-}
-window.addEventListener('scroll', scrollActive)
-
-/*===== SCROLL REVEAL ANIMATION =====*/
-const sr = ScrollReveal({
+  // Remove menu on mobile
+  const navLink = document.querySelectorAll('.nav__link');
+  
+  function linkAction() {
+    const navMenu = document.getElementById('nav-menu');
+    navMenu.classList.remove('show');
+  }
+  navLink.forEach((n) => n.addEventListener('click', linkAction));
+  
+  // Scroll sections active link
+  const sections = document.querySelectorAll('section[id]');
+  
+  function scrollActive() {
+    const scrollY = window.pageYOffset;
+  
+    sections.forEach((current) => {
+      const sectionHeight = current.offsetHeight;
+      const sectionTop = current.offsetTop - 50;
+      const sectionId = current.getAttribute('id');
+  
+      if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+        document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active');
+      } else {
+        document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active');
+      }
+    });
+  }
+  window.addEventListener('scroll', scrollActive);
+  
+  // Scroll reveal animation
+  const sr = ScrollReveal({
     origin: 'top',
     distance: '60px',
     duration: 1500,
     delay: 200,
-        reset: true
-});
-
-sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text', {});
-sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img, .about__details', { delay: 300 });
-sr.reveal('.home__social-icon', { interval: 200 });
-sr.reveal('.skills__data, .work__img, .contact__input', { interval: 200 }); 
+    reset: true
+  });
+  
+  sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text', {});
+  sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img, .about__details', { delay: 300 });
+  sr.reveal('.home__social-icon', { interval: 200 });
+  sr.reveal('.skills__data, .work__img, .contact__input', { interval: 200 });
+  
