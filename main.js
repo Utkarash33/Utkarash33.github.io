c1c1e7ea45439e9ab060ac67214a444496f8bf8d
atulthakur3303@gmail.com <atulthakur3303@gmail.com>
2023-07-31 22:19:28 +0530

    nav bar home section try to fix

--- main.js
+++ main.js
@@ -82,5 +82,5 @@ const showMenu = (toggleId, navId) => {
   sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text', {});
   sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img, .about__details', { delay: 300 });
   sr.reveal('.home__social-icon', { interval: 200 });
-  sr.reveal('.skills__data, .work__img, .contact__input', { interval: 200 });
+  sr.reveal('.skills__data, .work__img', { interval: 200 });
   
