5a65b9343a718359cfb0f8c323cd4e7637b14182
atulthakur3303@gmail.com <atulthakur3303@gmail.com>
2023-08-07 22:55:25 +0530

    test

--- main.js
+++ main.js
@@ -76,9 +76,8 @@ const showMenu = (toggleId, navId) => {
     delay: 200,
     reset: true
   });
-  
-  sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text', {});
-  sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img, .about__details', { delay: 300 });
-  sr.reveal('.home__social-icon', { interval: 200 });
-  sr.reveal('.skills__data, .work__img', { interval: 200 });
+  // sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text', {});
+  // sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img, .about__details', { delay: 300 });
+  // sr.reveal('.home__social-icon', { interval: 200 });
+  // sr.reveal('.skills__data, .work__img', { interval: 200 });
   
