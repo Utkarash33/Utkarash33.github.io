ea5a9885262168d57e8c3880e487721b3709da50
atulthakur3303@gmail.com <atulthakur3303@gmail.com>
2023-09-12 16:13:53 +0530

    adding the 4th project

--- main.js
+++ main.js
@@ -66,18 +66,18 @@ const showMenu = (toggleId, navId) => {
       }
     });
   }
-  // window.addEventListener('scroll', scrollActive);
+  window.addEventListener('scroll', scrollActive);
   
-  // // Scroll reveal animation
-  // const sr = ScrollReveal({
-  //   origin: 'top',
-  //   distance: '60px',
-  //   duration: 1500,
-  //   delay: 200,
-  //   reset: true
-  // });
-  // sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text', {});
-  // sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img, .about__details', { delay: 300 });
-  // sr.reveal('.home__social-icon', { interval: 200 });
-  // sr.reveal('.skills__data, .work__img', { interval: 200 });
+  // Scroll reveal animation
+  const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 1500,
+    delay: 200,
+    reset: true
+  });
+  sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text', {});
+  sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img, .about__details', { delay: 300 });
+  sr.reveal('.home__social-icon', { interval: 200 });
+  sr.reveal('.skills__data, .work__img', { interval: 200 });
   
